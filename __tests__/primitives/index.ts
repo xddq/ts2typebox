38cfcf3c8f581f1e7d9ef61d119b513f60cd8344
Pierre Dahmani <hi@pierre-dev.com>
2023-04-23 08:22:15 +0200

    remove jest, use node:test, start adding tests.

--- __tests__/primitives/index.ts
+++
@@ -1,6 +0,0 @@
-// import { TypeScriptToTypeBox } from "@/typescript-to-typebox";
-describe("Generating typebox based on primitive types", () => {
-  it("runs dummy test", async () => {
-    expect(true).toBe(true);
-  });
-});
